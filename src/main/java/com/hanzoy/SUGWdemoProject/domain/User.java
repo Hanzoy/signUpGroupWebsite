10f30d244794344c2f3d52366a7631e4d8b1319d
大小姐 <60333385+Hanzoy@users.noreply.github.com>
2021-01-25 15:49:23 +0800

    update

---
+++ src/main/java/com/hanzoy/SUGWdemoProject/domain/User.java
@@ -0,0 +1,57 @@
+package com.hanzoy.SUGWdemoProject.domain;
+
+public class User {
+    private Integer id;
+
+    private String username;
+
+    private String password;
+
+    private Boolean role;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Boolean getRole() {
+        return role;
+    }
+
+    public void setRole(Boolean role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", username=").append(username);
+        sb.append(", password=").append(password);
+        sb.append(", role=").append(role);
+        sb.append("]");
+        return sb.toString();
+    }
+}
---
+++ src/main/java/com/hanzoy/SUGWdemoProject/mapper/UserMapper.java
@@ -0,0 +1,30 @@
+package com.hanzoy.SUGWdemoProject.mapper;
+
+import com.hanzoy.SUGWdemoProject.domain.User;
+import com.hanzoy.SUGWdemoProject.domain.UserExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface UserMapper {
+    long countByExample(UserExample example);
+
+    int deleteByExample(UserExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    List<User> selectByExample(UserExample example);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") User record, @Param("example") UserExample example);
+
+    int updateByExample(@Param("record") User record, @Param("example") UserExample example);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
